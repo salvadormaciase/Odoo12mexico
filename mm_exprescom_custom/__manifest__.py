e20993b5b542c93908e09125c10c0d899f3a3799
mendoza03 <miguel.93.mendoza.s@hotmail.com>
2021-02-24 13:49:40 -0600

    Agrega reporte vencidos por cobrar en dolars v1

--- mm_exprescom_custom/__manifest__.py
+++ mm_exprescom_custom/__manifest__.py
@@ -22,9 +22,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        #'views/views.xml',
-        #'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/view_report_dollar.xml',
     ],
     "installable": True,
 }
