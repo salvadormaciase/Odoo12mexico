247a6355a91fe325e129553856944719246e502c
felipearizaga <felipearizaga@mit-mut.com>
2021-03-08 12:57:40 -0600

    Agrega Módulo Gandalf 12.0

---
+++ gandalf/models/__init__.py
@@ -0,0 +1,7 @@
+from . import account
+from . import account_invoice
+from . import account_move
+from . import account_move_line
+from . import account_revaluation_ledger
+from . import res_company
+from . import res_config
