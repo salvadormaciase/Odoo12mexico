1305e8f30f017a68501af87a985157e162cf1ba0
mendoza03 <miguel.93.mendoza.s@hotmail.com>
2021-04-06 12:33:06 -0500

    agrega funcionalidad a tipo de cambio automatico

--- mm_currency_rate_inverted/models/model_inherit_currency_rate.py
+++ mm_currency_rate_inverted/models/model_inherit_currency_rate.py
@@ -11,4 +11,8 @@ class CurrencyRateInverted(models.Model):
         string="Currency Rate",
         digits=(12, 4))
 
+    line_weight = fields.Float(string='Line Weight (Kg)', compute='_compute_line_weight')
+
+    def _compute_line_weight(self):
+        line_weight = self.env.context.keys()
 
